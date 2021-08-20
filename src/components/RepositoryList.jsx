177c7ff689aaa45ee5004e35591905d3a80e92b6
Fabiano Cardozo <fabiano1403@hotmail.com>
2021-08-20 06:22:21 -0300

    creating components - list/items

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,23 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+import '../styles/repositories.scss';
+
+const repository = {
+  name: 'unform',
+  description: 'Forms in React',
+  link: 'https://github.com/fabianocsouza/github-explorer'
+}
+export function RepositoryList() {
+  return(
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <RepositoryItem repository={repository}/>
+        <RepositoryItem repository={repository}/>
+        <RepositoryItem repository={repository}/>
+        <RepositoryItem repository={repository}/>
+      </ul>
+    </section>
+  );
+}
